f474985878bf6acbecef84ad2d8bf25f45c2160e
clark3493 <37091734+clark3493@users.noreply.github.com>
2018-03-05 20:11:55 -0600

    File restructure
    Added src and test files and changed function names to CamelCase

---
+++ src/ComputeVelocities1.m
@@ -0,0 +1,35 @@
+function [ue,ve,we,ub,vb,wb] = ComputeVelocities1(V,phi,theta,psi)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Function Name:            ComputeVelocities1
+%   Function Author:          Steve Clark
+%   Class:                    AAE 568
+%   Term:                     Sp2018
+%
+%   SCRIPT DESCRIPTION:
+%       This functions converts the given velocity magnitude and aircraft
+%       heading into earth fixed axis velocity components. Quaternions are
+%       then computed from the heading components and compiled into a
+%       direction cosine matrix. The direction cosine matrix is then used
+%       to compute the body axis velocity components. Singularities in the
+%       Euler angle conversion matrix are avoided.
+%   
+%   NOTES:
+%       Input Euler angles should be in radians.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % compute the axis velocity components
+    ue = V * cos(theta) * cos(psi);
+    ve = V * cos(phi)   * sin(psi);
+    we =-V * sin(theta);
+    
+    % compute the direction cosine matrix
+    Cm = DirCosMat(phi,theta,psi);
+    
+    % compute body axis velocity components
+    earth_vel = [ue; ve; we];
+    body_vel = Cm * earth_vel;
+    
+    ub = body_vel(1);
+    vb = body_vel(2);
+    wb = body_vel(3);
