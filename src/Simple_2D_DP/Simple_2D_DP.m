036c64b038078ae53088e66eb600fac255554fb4
clark3493 <37091734+clark3493@users.noreply.github.com>
2018-03-26 20:51:18 -0500

    Update notebook w/ references

---
+++ src/Simple_2D_DP/Simple_2D_DP.m
@@ -0,0 +1,63 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Script Name:            Simple_2D_DP.m
+%   Script Author:          Steve Clark
+%
+%   SCRIPT DESCRIPTION
+%       This script solves a simplified dynamic programming problem for the
+%		missile project.
+%       - Only 2D dynamics are considered
+%       - Assumes that the missile is well controlled by arbitrary control 
+%         surface inputs such that the missile's pitch Euler angle can be 
+%         considered a control and control surface deflections are not 
+%         considered.
+%       - No inequality constraints (i.e. no limits on vehicle or control
+%         surface angle of attack
+%       - Constant thrust
+%       - Aerodynamic forces are not considered
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clearvars; close all;
+
+%% ----- USER INPUTS ------------------------------------------------------
+
+% initial conditions
+global x0 z0 xdot0 zdot0;
+x0 = 0.;        % x coord, absolute coordinates, meters
+z0 = -2000.;    % z coord, absolute coordinates, meters  (z = -altitude)
+xdot0 = 100.;   % x velocity, absolute coordinates, m/s
+zdot0 = 0.;     % z velocity, absolute coordinates, m/s
+
+% final conditions
+global X Z eta;
+X = 2000;       % target x absolute coordinates, meters
+Z = 0.;         % target z absolute coordinates, meters
+eta = 63.;      % desired impact angle relative to horizontal, deg
+
+% cost function weights
+global A B;
+A = 10.;         % impact angle
+B = 0.;         % time to target
+
+% constant parameters
+global m T g;
+m = 100.;       % kg
+T = 1000.;       % N
+g = 9.81;       % m/s^2
+
+% numerical solution parameters
+
+
+%% ----- END USER INPUTS --------------------------------------------------
+
+%% INITIALIZATION
+
+
+%% SOLUTION
+
+
+%% PLOTTING
+
+
+%% FUNCTION DEFINITIONS
+
+
